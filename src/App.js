eb6d3b6af80c46dd7f439458ed8c82b3cecbad6c
lenovo <lenovo@LAPTOP-BLNTEBH7>
2023-06-29 10:57:52 +0530

    chages for the data

--- src/App.js
+++ src/App.js
@@ -6,23 +6,36 @@ import Product from './Product';
 import RequireAuth from './private/RequireAuth'
 import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import Button from './Button';
+import Posts from './Posts';
+import NewPost from './NewPost';
+import Post from './Post';
 
 function App() {
   return (
     <BrowserRouter>
+     <div>entired common things</div>
 
     <Routes>
+
+     
 
     <Route path="/" element={<Login/>} />
     <Route path="/signup" element={<Signup/>} />
     <Route path="/productlist" element={<RequireAuth><Product/>
+
+
     
       </RequireAuth>}/>
+
+  <Route path='posts' element={<Posts />}>
+        <Route path='new' element={<NewPost />} /> 
+       <Route path=':postId' element={<Post />} /> 
+ </Route>
 
 
     </Routes>
 
-    <Button><b>This is mydata</b></Button>
+    
     </BrowserRouter>
   );
 }
---
+++ src/Posts.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+export default function Posts() {
+  return (
+
+    <div>
+    <h1>
+        
+     This is the menu
+
+
+    </h1>
+
+    <Outlet />
+
+    </div>
+  )
+}
